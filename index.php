ff78e57e0ad7c5b740b781074ee08e081a034949
joytiger <joiecwang@yahoo.com>
2014-08-19 20:06:53 -0400

    first

--- index.php
+++ index.php
@@ -12,8 +12,6 @@
 	
 	<!-- Styles -->
 	<link rel="stylesheet" type="text/css" media="all" href="./Chicago Lobbyists_files/master.css">
-
-	<!-- JavaScript -->
   <script type="text/javascript" async="" src="./Chicago Lobbyists_files/ga.js"></script><script src="./Chicago Lobbyists_files/jquery.min.js" type="text/javascript"></script>
   <script src="./Chicago Lobbyists_files/jquery-ui-1.8.16.custom.min.js" type="text/javascript"></script>
 	<script src="./Chicago Lobbyists_files/jquery.ezpz_hint.min.js" type="text/javascript"></script>
